227c55fb62caaf168272d123fdd77b044d9d3761
AleksMoistus <a.moistus@gmail.com>
2022-02-13 22:41:16 +0200

    js

--- Failid/JS-13.js
+++ Failid/JS-13.js
@@ -1,18 +1,29 @@
 let username = ''
 while(username == '')
 {
-    username = prompt("Enter your name:")
+    username = prompt("Enter your name: ")
 }
 
-let hello = "Tere <strong>" + username + "</strong>"
+let hello = "Hello <strong>" + username + "</strong>. "
 let helloElement = document.getElementById("hello")
+let nameShort = "You have a short name. "
+let nameLong = "You have a long name. "
 
 if (username == "Aleks") 
 {
-    hello = "Tere <strong>Boss</strong>"
+    hello = "Hello <strong>Boss</strong>. "
 }
 
-hello = hello + "<h2>Letters in your name</h2>"
+if(username.length < [8])
+{
+    hello = hello + nameShort
+}
+else
+{
+    hello = hello + nameLong
+}
+
+hello = hello + "<h2>Letters in your name:</h2>"
 
 for (let index = 0; index < username.length; index++)
 {
